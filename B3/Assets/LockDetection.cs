e2e036ad5e6abeb18174de9f6d9b4165a2ed4253
ClementineCurel <78617457+Clementine951@users.noreply.github.com>
2022-03-29 14:43:56 +0200

    Update Scene reTestDoor
    Add lock on a door

---
+++ B3/Assets/LockDetection.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockDetection : MonoBehaviour
+{
+    public GameObject card;
+    public GameObject cube;
+
+    public string tagToCompare = "AccessCard";
+
+
+    public void OnTriggerEnter(Collider col)
+    {
+        //card.SetActive(true);
+
+        if (col.transform.tag == tagToCompare)
+        {
+            cube.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+    private void Awake()
+    {
+        //card.SetActive(false);
+        cube.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
