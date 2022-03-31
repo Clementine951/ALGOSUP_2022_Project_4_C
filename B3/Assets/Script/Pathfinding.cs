4a77daefbd2e21f941bc46ae87a36bd39be37ee1
Malo Archimbaud <malo.archimbaud@algosup.com>
2022-03-31 11:08:23 +0200

    Update Characters
    Stop when they are arrived

--- B3/Assets/Script/Pathfinding.cs
+++ B3/Assets/Script/Pathfinding.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class Pathfinding : MonoBehaviour
 {
     public Transform goal;
+    public GameObject Character;
     public Animator anim;
 
     // Start is called before the first frame update
@@ -14,7 +15,12 @@ public class Pathfinding : MonoBehaviour
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.destination = goal.position;
         anim.SetBool("Path", true);
+        
     }
     // Update is called once per frame
-
+    private void Update()
+    {
+        if (Character.transform.position.y == goal.position.x || Character.transform.position.z == goal.position.z)
+            anim.SetBool("Path", false);
+    }
 }
